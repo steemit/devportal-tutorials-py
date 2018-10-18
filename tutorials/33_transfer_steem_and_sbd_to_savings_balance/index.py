fab554373cd97f66aa737d78a4e4c12259e103af
shiftb1t <git@shiftbit.io>
2018-10-18 11:04:35 +0200

    upd readme and code to include `withdrawal` function as per JS-T AC

--- tutorials/33_transfer_steem_and_sbd_to_savings_balance/index.py
+++ tutorials/33_transfer_steem_and_sbd_to_savings_balance/index.py
@@ -1,6 +1,7 @@
 import steembase
 import steem
 from pick import pick
+import random
 
 # connect to testnet
 steembase.chains.known_chains['STEEM'] = {
@@ -9,8 +10,8 @@ steembase.chains.known_chains['STEEM'] = {
 }
 
 #capture user information
-username = input('Enter username: ') #demo account: cdemo
-wif = input('Enter private ACTIVE key: ') #demo account: 5KaNM84WWSqzwKzY82fXPaUW43idbLnPqf5SfjGxLfw6eV2kAP3
+username = input('Enter username: ') #demo account: demo01
+wif = input('Enter private ACTIVE key: ') #demo account: 5HxTntgeoLm4trnTz94YBsY6MpAap1qRVXEKsU5n1v2du1gAgVH
 
 #connect node and private active key
 client = steem.Steem(nodes=['https://testnet.steem.vc'], keys=[wif])
@@ -22,39 +23,57 @@ if(userinfo is None) :
     exit()
 total_steem = userinfo['balance']
 total_sbd = userinfo['sbd_balance']
+savings_steem = userinfo['savings_balance']
+savings_sbd = userinfo['savings_sbd_balance']
 
 print('CURRENT ACCOUNT BALANCE:' + '\n' + total_steem + '\n' + total_sbd + '\n')
+print('CURRENT SAVINGS BALANCE:' + '\n' + savings_steem + '\n' + savings_sbd + '\n')
 
 input('Press enter to continue with the transfer' + '\n')
 
 
-#choice of transfer
-title = 'Please choose transfer type: '
-options = ['STEEM', 'SBD', 'Cancel Transfer']
-# get index and selected transfer type
-option, index = pick(options, title)
+#choice of transfer/withdrawal
+title1 = 'Please choose transfer type: '
+options1 = ['Transfer', 'Withdrawal', 'Cancel']
+#get index and selected transfer type
+transfer_type, index = pick(options1, title1)
 
-if option == 'Cancel Transfer':
+if transfer_type == 'Cancel':
     print('Transaction cancelled')
     exit()
+
+#choice of currency
+title2 = 'Please choose currency: '
+options2 = ['STEEM', 'SBD']
+# get index and selected currency
+option, index = pick(options2, title2)
+
+if option == 'STEEM':
+    #get STEEM transfer amount
+    amount = input('Enter amount of STEEM to transfer: ')
+    asset = 'STEEM'
 else:
-    if option == 'STEEM':
-        #get STEEM transfer amount
-        amount = input('Enter amount of STEEM to transfer to savings: ')
-        asset = 'STEEM'
-    else:
-        #get SBD transfer amount
-        amount = input('Enter amount of SBD to transfer to savings: ')
-        asset = 'SBD'
+    #get SBD transfer amount
+    amount = input('Enter amount of SBD to transfer: ')
+    asset = 'SBD'
 
-#parameters: amount, asset, memo, to, account
-client.transfer_to_savings(float(amount), asset, '', username, username)
-
-print('\n' + 'Transfer to savings balance successful')
+if transfer_type == 'Transfer':
+    #parameters: amount, asset, memo, to, account
+    client.transfer_to_savings(float(amount), asset, '', username, username)
+    print('\n' + 'Transfer to savings balance successful')
+else:
+    #create request ID random integer
+    requestID = random.randint(1,1000000)
+    #parameters: amount, asset, memo, request_id=None, to=None, account=None
+    client.transfer_from_savings(float(amount), asset, '', requestID, username, username)
+    print('\n' + 'Withdrawal from savings successful, transaction ID: ' + str(requestID))
 
 #get remaining account balance for STEEM and SBD
 userinfo = client.get_account(username)
 total_steem = userinfo['balance']
 total_sbd = userinfo['sbd_balance']
+savings_steem = userinfo['savings_balance']
+savings_sbd = userinfo['savings_sbd_balance']
 
 print('\n' + 'REMAINING ACCOUNT BALANCE:' + '\n' + total_steem + '\n' + total_sbd + '\n')
+print('CURRENT SAVINGS BALANCE:' + '\n' + savings_steem + '\n' + savings_sbd + '\n')
